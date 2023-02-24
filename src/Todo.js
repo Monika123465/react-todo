46f5235924ded2c63114c2e0744a6f19113a2c64
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-02-24 21:47:11 +0530

    css complete

--- src/Todo.js
+++ src/Todo.js
@@ -5,6 +5,7 @@ const Todo = () => {
   const [todo, setTodo] = useState([])
 
   const oninputChange = (e) => {
+  
     setInput(e.target.value)
   }
 
@@ -43,25 +44,27 @@ const Todo = () => {
 
   return (
     <>
-      <form onSubmit={onformsubmit}>
-        <h1>Todo</h1>
-        <input type='text' placeholder='Add Todo' onChange={oninputChange} />
-        <button type='submit'>Add Todo </button>
+    <div  style={{width:"50%" ,margin:"auto",marginTop:'100px',boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',backgroundColor:"skyblue",color:'white',paddingBottom:"40px"}}>
+      <form onSubmit={onformsubmit} style={{marginBottom:"40px"}}>
+        <h1 style={{fontSize:"25px",margin:"40px",paddingTop:"40px"}}>Todo</h1>
+        <input style={{width:"400px",height:'30px',color:"black"}} type='text' placeholder='Add Todo' onChange={oninputChange} />
+        <button style={{width:"100px",height:"40px",border:"1px solid blueviolet",marginLeft:'20px',backgroundColor:"violet"}} type='submit'>Add Todo </button>
       </form>
 
       {
         todo.map(el => {
-          return <div key={el.id}>
-            <h1>{el.text} </h1>
-            <button onClick={() => {handleUpdate(el.id)}}>{el.isDone ? "completed" : "not completed"}</button>
-            <button onClick={()=>{handleDelete(el.id)}}>Remove Todo</button>
+          return <div key={el.id} style={{display:"flex",gap:"40px",margin:"auto",marginBottom:"20px"}}>
+            <h1 style={{width:"400px",marginLeft:"60px"}}>{el.text} </h1>
+            <button style={{width:"200px",height:"40px",border:"1px solid blueviolet",marginLeft:'20px',backgroundColor:"violet"}} onClick={() => {handleUpdate(el.id)}}>{el.isDone ? "completed" : "not completed"}</button>
+            <button style={{width:"120px",height:"40px",border:"1px solid blueviolet",marginLeft:'20px',backgroundColor:"violet"}} onClick={()=>{handleDelete(el.id)}}>Remove Todo</button>
           </div>
 
 
 
 
         })
-      }
+      }</div>
+
     </>
   )
 }
